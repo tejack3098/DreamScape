08c3086e81eadacf051113f5d1704781b22cf210
Tejas Choudhari <tejas.choudhari36@gmail.com>
2024-09-02 17:42:46 -0400

    Added Villa Controller
    Add crud functionality for Villa

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,21 @@ namespace WhiteLagoon.Domain.Entities
     {
         public int Id { get; set; }
 
+        [MaxLength(60)]
         public required string Name { get; set; }
 
         public string? Description { get; set; }
 
+        [Display(Name = "Price Per Night")]
+        [Range(10,10000)]
         public double Price { get; set; }
 
         public int Sqft { get; set; }
 
+        [Range(1,10)]
         public int Occupancy { get; set; }
 
+        [Display(Name = "Image Url")]
         public string? ImageUrl {  get; set; }
 
         public DateTime? Created_Date { get; set; }
