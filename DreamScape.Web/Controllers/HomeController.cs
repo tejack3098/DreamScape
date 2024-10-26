0dfbdccc0b3ba831a14f047e670cec2478f3bfe8
Tejas Choudhari <tejas.choudhari36@gmail.com>
2024-10-26 18:03:17 -0400

    Added VillaList partial View

--- DreamScape.Web/Controllers/HomeController.cs
+++ DreamScape.Web/Controllers/HomeController.cs
@@ -40,6 +40,26 @@ namespace DreamScape.Web.Controllers
             return View(homeVM);
         }
 
+        public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
+        {
+            var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
+            foreach (var villa in villaList)
+            {
+                if (villa.Id % 2 == 0)
+                {
+                    villa.IsAvailable = false;
+                }
+            }
+            HomeVM homeVM = new()
+            {
+                CheckInDate = checkInDate,
+                VillaList = villaList,
+                Nights = nights
+            };
+
+            return PartialView("_VillaList", homeVM);
+        }
+
         public IActionResult Privacy()
         {
             return View();
