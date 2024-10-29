f18a0873b5717a6f7abfc0c0d79a643082deb6ea
Tejas Choudhari <tejas.choudhari36@gmail.com>
2024-10-29 12:42:26 -0400

    Added Admin Dashboard

--- DreamScape.Web/Controllers/AccountController.cs
+++ DreamScape.Web/Controllers/AccountController.cs
@@ -135,15 +135,22 @@ namespace DreamScape.Web.Controllers
                     .PasswordSignInAsync(loginVM.Email, loginVM.Password, loginVM.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
+                    var user = await _userManager.FindByEmailAsync(loginVM.Email);
 
-
-                    if (string.IsNullOrEmpty(loginVM.RedirectUrl))
+                    if (await _userManager.IsInRoleAsync(user, SD.Role_Admin))
                     {
-                        return RedirectToAction("Index", "Home");
+                        return RedirectToAction("Index", "Dashboard");
                     }
                     else
                     {
-                        return LocalRedirect(loginVM.RedirectUrl);
+                        if (string.IsNullOrEmpty(loginVM.RedirectUrl))
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                        else
+                        {
+                            return LocalRedirect(loginVM.RedirectUrl);
+                        }
                     }
                 }
                 else
