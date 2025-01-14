84ea2e8a4be3dfe2518e1365821933916143e338
Tejas Choudhari <tejas.choudhari36@gmail.com>
2025-01-14 00:57:03 -0500

    Added Radial Chart

--- DreamScape.Web/Controllers/DashboardController.cs
+++ DreamScape.Web/Controllers/DashboardController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DreamScape.Application.Common.Interfaces;
+using DreamScape.Application.Common.Utility;
+using DreamScape.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DreamScape.Web.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        static int previousMonth = DateTime.Now.Month == 1 ? 12 : DateTime.Now.Month - 1;
+        readonly DateTime previousMonthStartDate = new(DateTime.Now.Year, previousMonth, 1);
+        readonly DateTime currentMonthStartDate = new(DateTime.Now.Year, DateTime.Now.Month, 1);
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public async Task<IActionResult> GetTotalBookingRadialChartData()
+        {
+            var totalBookings = _unitOfWork.Booking.GetAll(u => u.Status != SD.StatusPending
+                                                                || u.Status == SD.StatusCancelled);
+            var countByCurrentMonth = totalBookings.Count(u => u.BookingDate >= currentMonthStartDate &&
+                                                               u.BookingDate <= DateTime.Now);
+            var countByPreviousMonth = totalBookings.Count(u => u.BookingDate >= previousMonthStartDate &&
+                                                                u.BookingDate <= currentMonthStartDate);
+
+            RadialBarChartVM radialBarChartVM = new();
+
+
+            int increaseDecreaseRatio = 100;
+
+            if (countByPreviousMonth != 0)
+            {
+                increaseDecreaseRatio = Convert.ToInt32((countByCurrentMonth - countByPreviousMonth) / countByPreviousMonth * 100);
+            }
+
+            radialBarChartVM.TotalCount = totalBookings.Count();
+            radialBarChartVM.CountInCurrentMonth = countByCurrentMonth;
+            radialBarChartVM.HasRatioIncreased = currentMonthStartDate > previousMonthStartDate;
+            radialBarChartVM.Series = new int[] { increaseDecreaseRatio };
+
+            return Json(radialBarChartVM);
+        }
+
     }
 }
---
+++ DreamScape.Web/ViewModels/RadialBarChartVM.cs
@@ -0,0 +1,13 @@
+﻿namespace DreamScape.Web.ViewModels
+{
+    public class RadialBarChartVM
+    {
+        public decimal TotalCount { get; set; }
+
+        public decimal CountInCurrentMonth { get; set; }
+
+        public bool HasRatioIncreased { get; set; }
+
+        public int[] Series { get; set; }
+    }
+}
